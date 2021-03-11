60348b49964b261edddbc1abf1be1c39cf58ef8b
e.shakeri <QWEasd123>
2021-03-11 12:06:27 +0330

    create project

---
+++ src/test/java/com/sn/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sn.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
